830af5f97bb966f1415028f93ccee748dc42f25b
trungthanh <trungthanhnt@gmail.com>
2018-05-30 10:10:59 +0700

    restructure

---
+++ models/kvobject.go
@@ -0,0 +1,26 @@
+
+package models
+
+import (
+	"fmt"
+)
+
+type  KVObject struct{
+	Key string 
+	Value string 
+}
+
+// type  KVObject struct{
+// 	Key string `json:key`
+// 	Value string `json:value`
+// }
+
+func init() {
+	fmt.Println("Hello world init KVOBject")
+}
+
+var (
+	// global key - value cache
+	KV = make (map[string]string )
+)
+
